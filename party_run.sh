b0efd36c15b0b19c077216acb95786a22a424c1e
Liquorice Ignignokt <lignigno@un-s1.kzn.21-school.ru>
2021-03-24 05:14:58 +0300

    fix party_run.sh

--- party_run.sh
+++ party_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-gcc party_parrot_dance_floor.c -o 07_03_21_in_memory_of_the_Kesha
-mv 07_03_21_in_memory_of_the_Kesha /bin/
+gcc party_parrot_dance_floor.c -o .07_03_21_in_memory_of_the_Kesha
+mv .07_03_21_in_memory_of_the_Kesha ~/.07_03_21_in_memory_of_the_Kesha
 
-echo -en "alias cd=\"07_03_21_in_memory_of_the_Kesha\"" >> ~/.zshrc
-echo -en "alias cd=\"07_03_21_in_memory_of_the_Kesha\"" >> ~/.bashrc
+echo -en "alias cd=\"~/.07_03_21_in_memory_of_the_Kesha\"" >> ~/.zshrc
+echo -en "alias cd=\"~/.07_03_21_in_memory_of_the_Kesha\"" >> ~/.bashrc
 rm -rf ../party_parrot_dance_floor/
