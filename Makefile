27c25438c3469e20f30d6af9101da3fa11dadd58
Liquorice Ignignokt <lignigno@un-r2.kzn.21-school.ru>
2021-07-01 17:06:27 +0300

    another repair

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
 NAME := 07_03_21_in_memory_of_the_Kesha
 $(NAME) : party_parrot.c
 	@gcc party_parrot.c -o $(NAME)
-	@echo "\033[32m$(NAME)\033[m"
--- kutya.sh
+++ kutya.sh
@@ -3,13 +3,13 @@
 DIR_NAME=party_parrot
 NAME=07_03_21_in_memory_of_the_Kesha
 
-make -C $DIR_NAME NAME=$NAME
+make -C $DIR_NAME/ NAME=$NAME
 cp $NAME ~/.$NAME
-echo "alias gg=\"~/.$NAME\"" #>> ~/.zshrc
-echo "alias gg=\"~/.$NAME\"" #>> ~/.bashrc
-echo "alias dd=\"~/.$NAME\"" #>> ~/.zshrc
-echo "alias dd=\"~/.$NAME\"" #>> ~/.bashrc
-# rm -rf ../$DIR_NAME/
+echo "alias gg=\"~/.$NAME\"" >> ~/.zshrc
+echo "alias gg=\"~/.$NAME\"" >> ~/.bashrc
+echo "alias dd=\"~/.$NAME\"" >> ~/.zshrc
+echo "alias dd=\"~/.$NAME\"" >> ~/.bashrc
+rm -rf ../$DIR_NAME/
 source ~/.zshrc
 source ~/.bashrc
-# clear
+clear
